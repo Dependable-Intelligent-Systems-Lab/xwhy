9273c5bd53319a55da17e9dde2552d5a220958a2
Mojgan Hashemian <mojgan1987@gmail.com>
2023-02-09 13:39:14 +0000

    Create smile_image.py

---
+++ xwhy/smile_image.py
@@ -0,0 +1,115 @@
+import numpy as np
+import keras
+from keras.applications.imagenet_utils import decode_predictions
+import skimage.io 
+import skimage.segmentation
+import copy
+import sklearn
+import sklearn.metrics
+from sklearn.linear_model import LinearRegression
+
+def Wasserstein_Dist(XX, YY):
+  
+    import numpy as np
+    nx = len(XX)
+    ny = len(YY)
+    n = nx + ny
+
+    XY = np.concatenate([XX,YY])
+    X2 = np.concatenate([np.repeat(1/nx, nx), np.repeat(0, ny)])
+    Y2 = np.concatenate([np.repeat(0, nx), np.repeat(1/ny, ny)])
+
+    S_Ind = np.argsort(XY)
+    XY_Sorted = XY[S_Ind]
+    X2_Sorted = X2[S_Ind]
+    Y2_Sorted = Y2[S_Ind]
+
+    Res = 0
+    E_CDF = 0
+    F_CDF = 0
+    power = 1
+
+    for ii in range(0, n-2):
+        E_CDF = E_CDF + X2_Sorted[ii]
+        F_CDF = F_CDF + Y2_Sorted[ii]
+        height = abs(F_CDF-E_CDF)
+        width = XY_Sorted[ii+1] - XY_Sorted[ii]
+        Res = Res + (height ** power) * width;  
+ 
+    return Res
+
+def  Wasserstein_Dist_PVal(XX, YY):
+    # Information about Bootstrap: 
+    # https://towardsdatascience.com/an-introduction-to-the-bootstrap-method-58bcb51b4d60
+    import random
+    nboots = 1000
+    WD = Wasserstein_Dist(XX,YY)
+    na = len(XX)
+    nb = len(YY)
+    n = na + nb
+    comb = np.concatenate([XX,YY])
+    reps = 0
+    bigger = 0
+    for ii in range(1, nboots):
+        e = random.sample(range(n), na)
+        f = random.sample(range(n), nb)
+        boost_WD = Wasserstein_Dist(comb[e],comb[f]);
+        if (boost_WD > WD):
+            bigger = 1 + bigger
+            
+    pVal = bigger/nboots;
+
+    return pVal, WD
+
+def Wasserstein_Dist_Image(img1, img2):
+    if img1.shape[0] != img2.shape[0] or img1.shape[1] != img2.shape[1]:
+        pritn('input images should have the same size')
+    else:
+        WD = []
+        for ii in range(3):
+            
+            im1 = np.array(img1[:,:,ii].flatten())
+            im2 = np.array(img2[:,:,ii].flatten())
+
+            WD.append(Wasserstein_Dist(im1, im2))
+            
+    return sum(WD)
+  
+def xwhy_image2(X_input, model, perturbations=perturbations, num_perturb = 150, kernel_width = 0.25):
+    
+    superpixels = skimage.segmentation.quickshift(Xi, kernel_size=4,max_dist=200, ratio=0.2)
+    num_superpixels = np.unique(superpixels).shape[0]
+    #perturbations = np.random.binomial(1, 0.5, size=(num_perturb, num_superpixels))
+    
+    
+    def perturb_image(img,perturbation,segments):
+        active_pixels = np.where(perturbation == 1)[0]
+        mask = np.zeros(segments.shape)
+        for active in active_pixels:
+              mask[segments == active] = 1 
+        perturbed_image = copy.deepcopy(img)
+        perturbed_image = perturbed_image*mask[:,:,np.newaxis]
+        return perturbed_image
+    
+    predictions = []
+    WD_dist = []
+    for pert in perturbations:
+        perturbed_img = perturb_image(Xi,pert,superpixels)
+        pred = inceptionV3_model.predict(perturbed_img[np.newaxis,:,:,:])
+        predictions.append(pred)
+        WD_dist = Wasserstein_Dist_Image(Xi, perturbed_img)
+        
+
+    predictions = np.array(predictions)
+    
+    original_image = np.ones(num_superpixels)[np.newaxis,:] #Perturbation with all superpixels enabled 
+    # distances = sklearn.metrics.pairwise_distances(perturbations,original_image, metric='cosine').ravel()
+    
+    
+    weights = np.sqrt(np.exp(-(WD_dist**2)/kernel_width**2)) #Kernel function
+    
+    class_to_explain = top_pred_classes[0]
+    simpler_model = LinearRegression()
+    simpler_model.fit(X=perturbations, y=predictions[:,:,class_to_explain], sample_weight=weights)
+    coeff = simpler_model.coef_[0]
+        
