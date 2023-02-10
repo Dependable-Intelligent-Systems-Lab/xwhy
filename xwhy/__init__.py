6f6bbd6a40d4730a54816ed1eee769198eb8c587
Mojgan Hashemian <mojgan1987@gmail.com>
2023-02-10 11:54:21 +0000

    Update __init__.py

--- xwhy/__init__.py
+++ xwhy/__init__.py
@@ -1,1 +1,8 @@
+import warnings
+import sys
 
+__version__ = '0.41.0'
+
+# check python version
+if (sys.version_info < (3, 0)):
+    warnings.warn("As of version 0.29.0 shap only supports Python 3 (not 2)!")
